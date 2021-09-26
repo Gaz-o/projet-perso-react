81eec08c4d3951f81cff4261a5204e1c894ffe64
Robuchon Hugues <hugues.robuchon@gmail.com>
2021-09-26 12:19:12 +0200

    amelioration du deroulement de l'immage

--- src/View/Main/Main.js
+++ src/View/Main/Main.js
@@ -10,36 +10,74 @@ import coif5 from '../img/coif5.jpg';
 function Main () {
     const [Zoom, setZoom] = useState(0.0012);
     const [Transp, setTransp] = useState(1);
-    const [Display, setDisplay] = useState("flex")
     const [Boucle, setBoucle] = useState(0)
     const Img = [coif1, coif2, coif3, coif4, coif5]
     const [Affiche, setAffiche] = useState(coif1)
+    const [Flow, setFlow] = useState("row-reverse")
+    const [Rotate, setRotate] = useState(-120)
+    const [MarginR, setMarginR] = useState(0)
+    const [MarginL, setMarginL] = useState(0)
 
     const changementImg = (e) => {
         let increment = e.nativeEvent.wheelDelta*0.0001;
         setBoucle(Boucle + increment)
-        setZoom(Zoom + increment);
+        setRotate(Rotate - increment*10);
         let rand = Math.floor(Math.random()*5)
-        if (Boucle > 2) {
+        setZoom(Zoom + increment);
+        if (Flow == "row-reverse") {
+            setRotate(Rotate + increment*100);
+        } else {
+            setRotate(Rotate - increment*100);
+        }
+        if (Boucle > 1.77) {
             setAffiche(Img[rand])
             setZoom(0.0012)
-            setTransp(1)
-            setDisplay("flex")
-            setBoucle(0)
-        } else if (Zoom > 1 && (Transp < 1 || Transp > 0)){
-            setTransp(Transp - increment)
+            setTransp(1);
+            setBoucle(0);
+            setMarginL(0)
+            setMarginR(0)
+            if (Flow == "row-reverse") {
+                setFlow("row");
+                setRotate(120)
+            } else {
+                setFlow("row-reverse");
+                setRotate(-120)
+            }
+        } else if (Boucle > 1.20 ){
+            setTransp(Transp - increment);
+            if (Flow == "row-reverse") {
+                setMarginR(MarginR - increment*300)
+                setRotate(Rotate + increment*100);
+            } else {
+                setMarginL(MarginL - increment*300)
+                setRotate(Rotate - increment*100);
+            }
         }
+        console.log(Boucle, Rotate);
     }
+
+    /*  transform: translateZ(0.5rem)
+        transform: rotateX(15deg) 
+        rotateY(-30deg);
+    */
     
     const classZoom = {
         zoom: Zoom,
         opacity: Transp,
-        display: Display
+        transform: "rotateY(" + Rotate + "deg)",
+        "margin-left": MarginL,
+        "margin-right": MarginR
+    }
+
+    const classGalerie = {
+        "flex-flow": Flow
     }
     
     return (
-        <div  className="galerie" onWheel={changementImg}>
-            <img src={Affiche} style={classZoom} />
+        <div className="conteneur3d">
+            <div  className="galerie" style={classGalerie} onWheel={changementImg}>
+                <img src={Affiche} style={classZoom} />
+            </div>
         </div>
     );
 }
